eaccd511dddeb63c60dabf0e8b0da9ec2ae830ff
Sandeep Gupta Koleti <36769399+sandeepkoleti@users.noreply.github.com>
2022-08-16 15:17:29 -0400

    Add files via upload

---
+++ 5 - algom - graphs - bfs.py
@@ -0,0 +1,10 @@
+def bfs(root):
+    queue = deque([root])
+    visited = set([root])
+    while len(queue) > 0:
+        node = queue.popleft()
+        for neighbor in get_neighbors(node):
+            if neighbor in visited:
+                continue
+            queue.append(neighbor)
+            visited.add(neighbor)
---
+++ 6 - algom - graphs - dfs.py
@@ -0,0 +1,6 @@
+def dfs(root, visited):
+    for neighbor in get_neighbors(root):
+        if neighbor in visited:
+            continue
+        visited.add(neighbor)
+        dfs(neighbor, visited)
